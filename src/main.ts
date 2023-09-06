99625dfb2079f4dcee3ed5d6cb877ecbbb86961b
Andre Miranda da Silva <amsilva3@gmail.com>
2023-09-06 18:19:15 +0200

    configuração certa cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET, POST, PUT, PATCH, DELETE HEAD, OPTIONS',
+    credentials: true,
+  });
   await app.listen(8080);
 }
 bootstrap();
