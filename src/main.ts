c2392f121b4fb6bae8f7a13c50da963f9b81c2e7
Andre Miranda da Silva <amsilva3@gmail.com>
2023-09-08 22:25:13 +0200

    testando cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const whitelist = [
+  'http://localhost:5173',
+  'http://localhost:8080',
+  'http://localhost:5000',
+  'http://127.0.0.1:5173',
+  'http://127.0.0.1:8080',
+  'http://127.0.0.1:5000',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: '*',
+    allowedHeaders: '*',
+    origin: whitelist,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
