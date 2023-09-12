1f9148c31e757440893659e0ccf1892160c5520d
Andre Miranda da Silva <amsilva3@gmail.com>
2023-09-12 22:47:47 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    allowedHeaders: ['content-type', 'Access-Control-Allow-Origin'],
-    origin: 'http://localhost:8080/user',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(8080);
 }
 bootstrap();
