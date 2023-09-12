23e0dbe10f7b6369478bfd1de0746ae83280eeac
Andre Miranda da Silva <amsilva3@gmail.com>
2023-09-12 21:26:44 +0200

    teste

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const whitelist = [
-  'http://localhost:5173',
-  'http://localhost:8080',
-  'http://localhost:5000',
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:8080',
-  'http://127.0.0.1:5000',
-];
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    allowedHeaders: '*',
-    origin: whitelist,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    preflightContinue: false,
+    allowedHeaders: ['content-type', 'Access-Control-Allow-Origin'],
+    origin: 'http://localhost:8080/user',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: true,
     optionsSuccessStatus: 204,
   });
   await app.listen(8080);
